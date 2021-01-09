d73c7daf9722eac9a206a85c168c68c0e1c8deb7
KapiCytryna <kacperczura9@gmail.com>
2021-01-09 10:53:45 +0100

    init commit

---
+++ src/main/java/pl/kab/carstogo/CarsToGoApplication.java
@@ -0,0 +1,13 @@
+package pl.kab.carstogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsToGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsToGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kab/carstogo/CarsToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kab.carstogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsToGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
